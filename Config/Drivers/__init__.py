2eea0f1b1afad886fab072b4d4a2df9118ffe4e3
gzosi <giova.franz96@gmail.com>
2026-06-23 19:00:13 +0200

    P1

--- Config/Drivers/__init__.py
+++ Config/Drivers/__init__.py
@@ -1,6 +1,6 @@
 #%% Importing Config Tools
 from .Phase0 import Phase0
-# from .Phase1 import Phase1
+from .Phase1 import Phase1
 # from .Phase2 import Phase2
 # from .Phase3 import Phase3
 # from .Phase4 import Phase4
@@ -8,7 +8,7 @@ from .Phase0 import Phase0
 class Drivers:
     class Phases:
         Phase0 = Phase0
-        # Phase1 = Phase1
+        Phase1 = Phase1
         # Phase2 = Phase2
         # Phase3 = Phase3
         # Phase4 = Phase4
