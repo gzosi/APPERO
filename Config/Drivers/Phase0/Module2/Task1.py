24d0e92f6299311c1e6d312ad305897dfc443e1b
gzosi <giova.franz96@gmail.com>
2026-06-23 17:34:38 +0200

    P0

--- Config/Drivers/Phase0/Module2/Task1.py
+++ Config/Drivers/Phase0/Module2/Task1.py
@@ -1,4 +1,3 @@
-#%% Importing Libreries
 import cv2 as cv
 #%% Defining Config Packet
 class Task1:
@@ -8,26 +7,27 @@ class Task1:
     class Settings:
         class Rotation:
             Camera1 = None
-            Camera2 = None
+            Camera2 = cv.ROTATE_180
+            # cv.ROTATE_90_CLOCKWISE, cv.ROTATE_180, cv.ROTATE_90_COUNTERCLOCKWISE 
         class Resync:
             class Database2:
-                Dataset1 = 67
+                Dataset1 = 7
             class Database3: 
-                class Dataset4:
-                    Background = 16
-                    Foreground = 22
+                class Dataset2:
+                    Background = 24
+                    Foreground = 25
             class Database4:
-                Dataset1 = 16
-                Dataset2 = 32
-                Dataset3 = 36
-                Dataset4 = 31
-                Dataset5 = 1
-                Dataset6 = 36
-                Dataset7 = 4
-                Dataset8 = 37
-                Dataset9 = 56
-                Dataset10 = 15
-                Dataset11 = 0
+                Dataset1 = 30
+                Dataset2 = 4
+                Dataset3 = 21
+                Dataset4 = 41
+                Dataset5 = 23
+                Dataset6 = 32
+                Dataset7 = 59
+                Dataset8 = 15
+                Dataset9 = 12
+                Dataset10 = 55
+                Dataset11 = 34
     class General:
         Activation = True
         Maker = True
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Test Phase 0 
 
 P1 done
-
+P2 done
